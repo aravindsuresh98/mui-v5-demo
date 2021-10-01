f194e2ceab342c3cb941f558fa999c76891ebefc
aravindsuresh98 <official.aravindsuresh98@gmail.com>
2021-10-01 18:35:39 +0530

    Add starter code for mui data tables

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ServerFilterGrid from "./ServerFilterGrid";
+
+function App() {
+	return (
+		<div className="App">
+			<ServerFilterGrid />
+		</div>
+	);
+}
+
+export default App;
